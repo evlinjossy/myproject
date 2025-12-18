b6eefe0ef76f379aebb7949ec6c54e85c8352714
Evlin <149107681+evlinjossy@users.noreply.github.com>
2025-12-19 03:21:10 +0530

    Delete crud directory

--- crud/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import studentList, studentDetail
-urlpatterns = [
-    path('students/', studentList.as_view(), name='student-list'),
-    path('students/<int:id>/', studentDetail.as_view(), name='student-detail'),
-]
